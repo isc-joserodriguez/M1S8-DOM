4408d4cdbd3841b4ac96c93672c4b2d4b9449abf
José Rodriguez <isc.joserodriguez@gmail.com>
2021-09-11 09:04:22 -0600

    Manipulacion del DOM

---
+++ main.js
@@ -0,0 +1,17 @@
+let parrafo = document.createElement('p');
+let texto = document.createTextNode(`
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores soluta ipsum culpa adipisci eligendi ad
+            neque laborum, sapiente in, unde, nesciunt quaerat delectus vel blanditiis laudantium nostrum debitis!
+            Eligendi, placeat!
+`);
+parrafo.appendChild(texto)
+document.getElementById('divisor').appendChild(parrafo);
+
+
+divisor.innerHTML = `
+<h1>Hola</h1>
+`
+
+divisor.innerHTML = divisor.innerHTML + `
+<a href="#">Hola</a>
+`
